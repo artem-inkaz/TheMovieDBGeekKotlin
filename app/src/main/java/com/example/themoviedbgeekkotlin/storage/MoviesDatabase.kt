2a483696ed385dd1d5c6a54e06bfe424e451f2f1
artemternovenko <artem.inkaz@gmail.com>
2021-06-10 13:49:57 +0600

    [ADDED] MoviesDatabase.kt

--- app/src/main/java/com/example/themoviedbgeekkotlin/storage/MoviesDatabase.kt
+++ app/src/main/java/com/example/themoviedbgeekkotlin/storage/MoviesDatabase.kt
@@ -1,4 +1,13 @@
 package com.example.themoviedbgeekkotlin.storage
 
-class MoviesDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.themoviedbgeekkotlin.storage.enteties.ActorEntity
+import com.example.themoviedbgeekkotlin.storage.enteties.MovieEntity
+
+@Database(entities = [MovieEntity::class, ActorEntity::class], version = 1, exportSchema = false)
+abstract class MoviesDatabase: RoomDatabase() {
+
+    abstract fun moviesDao(): MoviesDao
+    abstract fun actorsDao(): ActorsDao
 }
