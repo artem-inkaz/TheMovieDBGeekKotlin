a333f77ae9dddfeb0f651f2141e9dd657a50018c
artemternovenko <artem.inkaz@gmail.com>
2021-06-14 20:44:26 +0600

    [ADDED] Открытие страницы сайта с кодами классификации названия Языков по ISO_3166-1

--- app/src/main/java/com/example/themoviedbgeekkotlin/movielist/FragmentMovieList.kt
+++ app/src/main/java/com/example/themoviedbgeekkotlin/movielist/FragmentMovieList.kt
@@ -1,11 +1,17 @@
 package com.example.themoviedbgeekkotlin.movielist
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
@@ -33,6 +39,9 @@ class FragmentMovieList : Fragment(), OnItemViewClickListener {
 
     private val viewModel: FragmentMovieListViewModel by viewModels { MoviesListViewModelFactory() }
 
+    //переменная для хранения состояния доступности интернета
+    var networkAvailable = false
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -45,6 +54,16 @@ class FragmentMovieList : Fragment(), OnItemViewClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val moviesRecyclerView: RecyclerView = binding.movieListRecyclerView
+
+        var urlIso3166 = "https://ru.wikipedia.org/wiki/ISO_3166-1"
+        var urlIso639 = "https://snipp.ru/handbk/iso-639-1"
+        val webSettings = binding.webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.setAppCacheEnabled(false)
+
+        binding.searchLayout.iVHelp.setOnClickListener {
+            loadWebSite(binding.webView, urlIso3166,  requireContext())
+        }
 
         adapter2 = MoviesCategoriesAdapter(this)
         moviesRecyclerView.adapter = adapter2
@@ -146,10 +165,6 @@ class FragmentMovieList : Fragment(), OnItemViewClickListener {
         )
     }
 
-    companion object {
-        fun newInstance() = FragmentMovieList()
-    }
-
     override fun onItemViewClick(movie: Movie) {
         // сохранение при клике по записи MovieList
 //        viewModel.saveMoviesLocally(movie)
@@ -163,5 +178,74 @@ class FragmentMovieList : Fragment(), OnItemViewClickListener {
     }
 
     override fun onItemViewClickNotes(movie: MovieEntity) {
+    }
+
+    // будет принимать webview, адрес сайта  и контекст, и будет загружать сайт
+    private fun loadWebSite(webView: WebView, url: String, context: Context) {
+        binding.progressBar.visibility = View.VISIBLE
+        //Проверяем сетевое соединение
+        networkAvailable = isNetworkAvailable(context)
+        //очищаем кэш во избежание некоторых ошибок
+        webView.clearCache(true)
+        //Если соединение доступно, включаем видимость webview,
+        // присваиваем ему кастомный WebViewClient, и загружаем сайт.
+        // В случае отсутствия сети скрываем webview.
+        if (networkAvailable) {
+            // присваиваем ему кастоный WebViewClient
+            wvVisible(webView)
+            webView.loadUrl(url)
+//            webView.webViewClient = MyWebViewClient()
+        } else {
+            wvGone(webView)
+        }
+
+    }
+
+    //При наличии сети мы будем показывать webview и скрывать текстовое поле
+    private fun wvVisible(mWebView: WebView) {
+        mWebView.visibility = View.VISIBLE
+    }
+
+    //При отсутствии сети мы будем скрывать webview и отображать текстовое поле
+    private fun wvGone(mWebView: WebView) {
+        mWebView.visibility = View.GONE
+//        tvCheckConnection.visibility = View.VISIBLE
+        toastMessage("Возможная причина: Интернет-соединение отсутствует", requireContext())
+        binding.progressBar.visibility = View.GONE
+    }
+
+    //для проверки доступа к сети
+    //чтобы предупреждения устаревших методов не отображались
+    @Suppress("DEPRECATION")
+    private fun isNetworkAvailable(context: Context): Boolean {
+        try {
+            //инициализируем ConnectivityManager
+            val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            //для версий до и после 23 нужно применять разные методы класса ConnectivityManager
+            return if (Build.VERSION.SDK_INT > 22) {
+                //Если версия больше 22, используем метод activeNetwork для получения информации
+                // о сети по умолчанию, затем getNetworkCapabilities  для получения состояния
+                // текущего соединения, и hasCapability для проверки соединения
+                val an = cm.activeNetwork ?: return false
+                val capabilities = cm.getNetworkCapabilities(an) ?: return false
+                capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            } else {
+                //На более старых версиях будут использованы устаревшие методы activeNetworkInfo и isConnected
+                val a = cm.activeNetworkInfo ?: return false
+                a.isConnected && (a.type == ConnectivityManager.TYPE_WIFI || a.type == ConnectivityManager.TYPE_MOBILE)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return false
+    }
+
+    private fun toastMessage(message: String, context: Context) {
+
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    }
+
+    companion object {
+        fun newInstance() = FragmentMovieList()
     }
 }
